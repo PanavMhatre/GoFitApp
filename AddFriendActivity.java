8001d5b1f9f0f6f7942dfd9c77d0490eb717acdc
PanavMhatre <115958322+PanavMhatre@users.noreply.github.com>
2024-08-25 19:04:47 -0500

    Update AddFriendActivity.java

--- AddFriendActivity.java
+++ AddFriendActivity.java
@@ -101,10 +101,112 @@ public class AddFriendActivity extends AppCompatActivity {
         });
 
 
+    }
 
-
+    private void ChangeSystemElements() {
+        ImageView leftIcon = findViewById(R.id.backIcon);
+        TextView text = findViewById(R.id.activityText);
+        leftIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),Friend_Activity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+        text.setText("Add Friends");
+        if(Build.VERSION.SDK_INT>=21){
+            Window window = this.getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            window.setStatusBarColor(this.getResources().getColor(R.color.log_blue));
+        }
 
     }
+
+    private void loadUser(String s){
+        Query query = reference.orderByChild("name").startAt(s).endAt(s + "\uf8ff");
+        Log.d("TAG", String.valueOf(query));
+//        .orderByChild("name").startAt(s).endAt(s+"\uf8ff")
+        options=new FirebaseRecyclerOptions.Builder<UserDataRecView>().setQuery(query, UserDataRecView.class).build();
+        adapter = new FirebaseRecyclerAdapter<UserDataRecView, RecViewHolder>(options) {
+            @Override
+            protected void onBindViewHolder(@NonNull RecViewHolder holder, @SuppressLint("RecyclerView") final int position, @NonNull UserDataRecView model) {
+                String friend_key = getRef(position).getKey();
+                Log.d("TAG","" + friend_key);
+
+
+                    if(!fUser.getUid().equals(getRef(position).getKey().toString())){
+                        reference.child(friend_key).child("privateStatus").addValueEventListener(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                                String status = snapshot.getValue().toString();
+
+                                if(status.equals("private")){
+                                    holder.itemView.setLayoutParams(new ConstraintLayout.LayoutParams(0,0));
+                                }
+
+                                Log.d("TAG",status);
+                                if(status.equals("public")){
+                                    if(holder.profile!=null){
+                                        Picasso.get().load(model.getImage()).into(holder.profile);
+                                        holder.profile.setImageResource(R.drawable.profile);
+                                    }
+                                    holder.name.setText(model.getName());
+
+
+                                    holder.button.setOnClickListener(new View.OnClickListener() {
+                                        @Override
+                                        public void onClick(View v) {
+                                            Intent intent = new Intent(AddFriendActivity.this,PersonProfileActivity.class);
+                                            intent.putExtra("ID",getRef(position).getKey());
+                                            startActivity(intent);
+                                            overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+                                        }
+                                    });
+                                }
+
+
+
+                            }
+
+
+
+                            @Override
+                            public void onCancelled(@NonNull DatabaseError error) {
+
+                            }
+                        });
+
+
+
+
+                        } else{
+                            holder.itemView.setVisibility(View.GONE);
+                            holder.itemView.setLayoutParams(new RecyclerView.LayoutParams(0,0));
+                        }
+
+
+
+
+
+
+            }
+
+            @NonNull
+            @Override
+            public RecViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+
+                View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.row_users,parent,false);
+                return new RecViewHolder(view);
+            }
+        };
+
+        adapter.startListening();
+        recView.setAdapter(adapter);
+    }
+
 
     
 
