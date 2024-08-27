f5b44dfa4c92ffc899d631d32f33c20ffa07f1af
PanavMhatre <115958322+PanavMhatre@users.noreply.github.com>
2024-08-26 19:06:10 -0500

    Update Calender.java

--- Calender.java
+++ Calender.java
@@ -393,6 +393,80 @@ public class Calender extends AppCompatActivity {
             }
         });
 
+@Override
+    public void finish() {
+        super.finish();
+        overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right);
+    }
+
+    public static String getWeekDayName(String s) {
+        DateTimeFormatter dtfInput = DateTimeFormatter.ofPattern("u-M-d", Locale.ENGLISH);
+        DateTimeFormatter dtfOutput = DateTimeFormatter.ofPattern("EEEE", Locale.ENGLISH);
+        return LocalDate.parse(s, dtfInput).format(dtfOutput);
+    }
+
+    private void intilizeViews() {
+        day1 = findViewById(R.id.day1);
+        day2 = findViewById(R.id.day2);
+        day3 = findViewById(R.id.day3);
+        day4 = findViewById(R.id.day4);
+        day5 = findViewById(R.id.day5);
+        day6 = findViewById(R.id.day6);
+        day7 = findViewById(R.id.day7);
+        day8 = findViewById(R.id.day8);
+        day9 = findViewById(R.id.day9);
+        day10 = findViewById(R.id.day10);
+        day11 = findViewById(R.id.day11);
+        day12 = findViewById(R.id.day12);
+        day13 = findViewById(R.id.day13);
+        day14 = findViewById(R.id.day14);
+        day15 = findViewById(R.id.day15);
+        day16 = findViewById(R.id.day16);
+        day17 = findViewById(R.id.day17);
+        day18 = findViewById(R.id.day18);
+        day19 = findViewById(R.id.day19);
+        day20 = findViewById(R.id.day20);
+        day21 = findViewById(R.id.day21);
+        day22 = findViewById(R.id.day22);
+        day23 = findViewById(R.id.day23);
+        day24 = findViewById(R.id.day24);
+        day25 = findViewById(R.id.day25);
+        day26 = findViewById(R.id.day26);
+        day27 = findViewById(R.id.day27);
+        day28 = findViewById(R.id.day28);
+        day29 = findViewById(R.id.day29);
+        day30 = findViewById(R.id.day30);
+        day31 = findViewById(R.id.day31);
+        day32 = findViewById(R.id.day32);
+        day33 = findViewById(R.id.day33);
+        day34 = findViewById(R.id.day34);
+        day35 = findViewById(R.id.day35);
+        day36 = findViewById(R.id.day36);
+        day37 = findViewById(R.id.day37);
+        day38 = findViewById(R.id.day38);
+    }
+
+    private void ChangeSystemElements() {
+        ImageView leftIcon = findViewById(R.id.backIcon);
+        TextView text = findViewById(R.id.activityText);
+        leftIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),HomeActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+        text.setText("Calender");
+        if(Build.VERSION.SDK_INT>=21){
+            Window window = this.getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            window.setStatusBarColor(this.getResources().getColor(R.color.log_blue));
+        }
+
+    }
+
 
 
     }
